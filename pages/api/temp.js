cff267c3792bfee4b8e8a90a43d6b8a4c3070beb
Thomas Alexander Woolff <92778181+TWoolff@users.noreply.github.com>
2022-08-15 15:54:51 +0200

    ...

--- pages/api/temp.js
+++ pages/api/temp.js
@@ -1,12 +1,3 @@
-
-
-
-
-
-
-
-
-
 import {useEffect} from 'react'
 import parse from 'html-react-parser'
 import Link from 'next/link'
@@ -26,21 +17,51 @@ export const getServerSideProps = async ({params}) => {
 } 
 
 const Page = ({currentPage, currentChapter, ...props}) => {
-  const {pageId, text, input, is_snack, btn_link, btn_text, content} = currentPage
+  const {pageId, is_snack, btn_link, btn_text, content} = currentPage
   const {chapterId, title} = currentChapter
   const {setCurrentPage, setCurrentSnack, setNextLink, handleNavVisibility} = props
+
+  useEffect(() => {
+    setCurrentPage(pageId)
+    setNextLink(btn_link)
+    handleNavVisibility(true)
+    if (is_snack) {setCurrentSnack(true)} else {setCurrentSnack(false)}
+  }, [is_snack, pageId, btn_link])
 
   return ( 
     <section className='page'>
       <div className='page-info'>{chapterId} {title}</div>
-        {content.map((cont, i) => {
-          return (
-            <div key={i}>
-              {cont.text && parse(cont.text)}
-              {cont.input && <InputSelect placeholder={cont.input.placeholder} options={cont.input.options} btnTxt={btn_text} btn_link={btn_link} setNextLink={setNextLink} />}
-            </div>
-          )
-        })}
+      {content.map((cont, i) => {
+        return (
+          <div key={i}>
+            {cont.text && 
+              parse(cont.text)
+            }
+            {cont.input && cont.input.type === 'select' && 
+              <InputSelect placeholder={cont.input.placeholder} options={cont.input.options} btnTxt={btn_text} btn_link={btn_link} setNextLink={setNextLink} />
+            }
+            {cont.input && cont.input.type === 'text' && 
+              <InputText name={cont.input.name} placeholder={cont.input.placeholder} btnTxt={btn_text} btnLink={btn_link} />
+            }
+            {cont.input && cont.input.type === 'range' && 
+              <InputRange name={cont.input.name} min={cont.input.min} max={cont.input.max} label={cont.input.label} btnTxt={btn_text} btnLink={btn_link} />
+            }
+            {!cont.input &&
+              <div className='btn-container'>
+                  <Link href={btn_link}><a className='btn'>{btn_text}</a></Link>
+              </div>
+            }
+          </div>
+        )
+      })}
+      {is_snack && 
+        <>
+          <Background />
+          <div className='btn-container'>
+            <Link href={btn_link}><a className='btn'>{btn_text}</a></Link>
+          </div>
+        </>
+      }
     </section>
   )
 }
